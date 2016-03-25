695252ca606e1da21a47c15f52eaf83ec1a3b6db
MatiasE <mepifanio11@hotmail.com>
2016-03-25 17:52:48 -0300

    Segundo Commit

--- workspace/PrimerosPasos/src/poo/Uso_Vehiculo.java
+++ workspace/PrimerosPasos/src/poo/Uso_Vehiculo.java
@@ -1,11 +1,11 @@
 package poo;
-import javax.swing.*;
+// import javax.swing.*;
 
-public class Uso_Coche {
+public class Uso_Vehiculo {
 
 	public static void main(String[] args) {
 		
-		Coche Renault=new Coche(); // Instanciar una Clase. Ejemplar de Clase.
+		/* Coche Renault=new Coche(); // Instanciar una Clase. Ejemplar de Clase.
 		// Renault.largo=3000; Rompe Encapsulamiento.
 		Renault.establece_color(JOptionPane.showInputDialog("Introduce Color"));
 		// Renault.color="Rosa"; Rompe Encapsulamiento.
@@ -20,7 +20,22 @@ public class Uso_Coche {
 		
 		System.out.println(Renault.dime_peso_coche());
 		System.out.println("El precio final del coche es: " + Renault.precio_coche());
+		*/
 		
+		// Codigo Herencia.
+		
+		Coche micoche1=new Coche();
+		micoche1.establece_color("Rojo");
+		
+		Furgoneta mifurgoneta1=new Furgoneta(7, 580);
+		
+		mifurgoneta1.establece_color("Azul");
+		mifurgoneta1.configura_asientos("Si");
+		mifurgoneta1.configura_climatizador("Si");
+		
+		System.out.println(micoche1.dime_datos_generales() + " " + micoche1.dime_color());
+		
+		System.out.println(mifurgoneta1.dime_datos_generales() + " " + mifurgoneta1.dimeDatosFurgoneta());
 	}
 
 }
